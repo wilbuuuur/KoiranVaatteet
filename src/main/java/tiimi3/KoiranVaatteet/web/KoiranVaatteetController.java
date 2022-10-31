3061edfb686e1da1c2f789025baa840658a8f31c
divrrv <112497215+divrrv@users.noreply.github.com>
2022-10-31 11:17:57 +0200

    Poista

--- src/main/java/tiimi3/KoiranVaatteet/web/KoiranVaatteetController.java
+++ src/main/java/tiimi3/KoiranVaatteet/web/KoiranVaatteetController.java
@@ -3,6 +3,7 @@ package tiimi3.KoiranVaatteet.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,4 +19,11 @@ private KoiranVaatteetRepository vaateRepository;
 	model.addAttribute("vaatteet", vaateRepository.findAll());
 	return "vaatteet";
 }
+
+@RequestMapping(value = "/poista/{id}", method = RequestMethod.GET)
+public String deleteVaate (@PathVariable("id") Long vaateId, Model model) {
+	vaateRepository.deleteById(vaateId);
+	return "redirect:../vaatteet";
+}
+
 }
