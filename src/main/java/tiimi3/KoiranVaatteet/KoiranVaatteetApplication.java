90c46319ef46da95f6fbfc1952f3f0a4230de935
wilbuuuur <ville.aalto@myy.haaga-helia.fi>
2022-10-31 11:07:26 +0200

    controller

--- src/main/java/tiimi3/KoiranVaatteet/KoiranVaatteetApplication.java
+++ src/main/java/tiimi3/KoiranVaatteet/KoiranVaatteetApplication.java
@@ -9,5 +9,7 @@ public class KoiranVaatteetApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KoiranVaatteetApplication.class, args);
 	}
+	
+	
 
 }
--- src/main/java/tiimi3/KoiranVaatteet/web/KoiranVaatteetController.java
+++ src/main/java/tiimi3/KoiranVaatteet/web/KoiranVaatteetController.java
@@ -1,5 +1,21 @@
 package tiimi3.KoiranVaatteet.web;
 
-public class KoiranVaatteetController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import tiimi3.KoiranVaatteet.domain.KoiranVaatteetRepository;
+
+@Controller
+public class KoiranVaatteetController {
+@Autowired
+private KoiranVaatteetRepository vaateRepository;
+
+@RequestMapping(value = "/vaatteet", method = RequestMethod.GET)
+	public String list (Model model) {
+	model.addAttribute("vaatteet", vaateRepository.findAll());
+	return "vaatteet";
+}
 }
