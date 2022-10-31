8340427caeb0226a9aacda94028a9e9cc7aebad6
jesreu <jesse.reunamo@gmail.com>
2022-10-31 12:26:37 +0200

    Lisätty lisäys ominaisuus

--- src/main/java/tiimi3/KoiranVaatteet/web/KoiranVaatteetController.java
+++ src/main/java/tiimi3/KoiranVaatteet/web/KoiranVaatteetController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import tiimi3.KoiranVaatteet.domain.KoiranVaatteetRepository;
+import tiimi3.KoiranVaatteet.domain.Vaate;
 
 @Controller
 public class KoiranVaatteetController {
@@ -24,6 +25,18 @@ private KoiranVaatteetRepository vaateRepository;
 public String deleteVaate (@PathVariable("id") Long vaateId, Model model) {
 	vaateRepository.deleteById(vaateId);
 	return "redirect:../vaatteet";
+}
+
+@RequestMapping(value = "/lisaa")
+public String addVaate (Model model){
+	model.addAttribute("vaate", new Vaate());
+    return "lisaa";
+}
+
+@RequestMapping(value = "/tallenna", method = RequestMethod.POST)
+public String save(Vaate vaate){
+    vaateRepository.save(vaate);
+    return "redirect:vaatteet";
 }
 
 }
