0205005d68475bbb4eb070a7f27404b7b35b98ab
divrrv <112497215+divrrv@users.noreply.github.com>
2022-10-31 09:51:32 +0200

    to english

--- src/main/java/tiimi3/KoiranVaatteet/domain/KoiranVaatteet.java
+++ src/main/java/tiimi3/KoiranVaatteet/domain/KoiranVaatteet.java
@@ -2,59 +2,67 @@ package tiimi3.KoiranVaatteet.domain;
 
 public class KoiranVaatteet {
 	private long id;
-	private String nimi;
-	private String tyyppi;
-	private double hinta;
-	private String valmistaja;
+	private String name;
+	private String type;
+	private double price;
+	private String manufacturer;
 	
 	public KoiranVaatteet () {
 		
 	}
 	
-	public KoiranVaatteet (String nimi, String tyyppi, double hinta, String valmistaja) {
+	public KoiranVaatteet (String name, String type, double price, String manufacturer) {
 		super();
-		this.nimi = nimi;
-		this.tyyppi = tyyppi;
-		this.hinta = hinta;
-		this.valmistaja = valmistaja;			
+		this.name = name;
+		this.type = type;
+		this.price = price;
+		this.manufacturer = manufacturer;			
 	}
-	
-	
+
 	public long getId() {
 		return id;
 	}
-	public String getNimi() {
-		return nimi;
+
+	public String getName() {
+		return name;
 	}
-	public String getTyyppi() {
-		return tyyppi;
+
+	public String getType() {
+		return type;
 	}
-	public double getHinta() {
-		return hinta;
+
+	public double getPrice() {
+		return price;
 	}
-	public String getValmistaja() {
-		return valmistaja;
+
+	public String getManufacturer() {
+		return manufacturer;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
-	public void setNimi(String nimi) {
-		this.nimi = nimi;
+
+	public void setName(String name) {
+		this.name = name;
 	}
-	public void setTyyppi(String tyyppi) {
-		this.tyyppi = tyyppi;
+
+	public void setType(String type) {
+		this.type = type;
 	}
-	public void setHinta(double hinta) {
-		this.hinta = hinta;
+
+	public void setPrice(double price) {
+		this.price = price;
 	}
-	public void setValmistaja(String valmistaja) {
-		this.valmistaja = valmistaja;
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
 	}
 
 	@Override
 	public String toString() {
-		return "KoiranVaatteet [id=" + id + ", nimi=" + nimi + ", tyyppi=" + tyyppi + ", hinta=" + hinta
-				+ ", valmistaja=" + valmistaja + "]";
-	}	
-
+		return "KoiranVaatteet [id=" + id + ", name=" + name + ", type=" + type + ", price=" + price + ", manufacturer="
+				+ manufacturer + "]";
+	}
+	
 }
--- src/main/java/tiimi3/KoiranVaatteet/domain/KoiranVaatteetRepository.java
+++ src/main/java/tiimi3/KoiranVaatteet/domain/KoiranVaatteetRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface KoiranVaatteetRepository extends CrudRepository<KoiranVaatteet, Long> {
-	List<KoiranVaatteet> findByNimi(String nimi);
+	List<KoiranVaatteet> findByName(String name);
 
 }
