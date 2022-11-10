43bb12ed49dd561778d5cd40d8673ce2c4b22a25
Leo Rautio <leoaleksisrautio@gmail.com>
2022-11-10 12:19:48 +0200

    valmistajat sivun ominaisuudet lisätty

--- src/main/java/tiimi3/KoiranVaatteet/web/ValmistajaController.java
+++ src/main/java/tiimi3/KoiranVaatteet/web/ValmistajaController.java
@@ -3,9 +3,12 @@ package tiimi3.KoiranVaatteet.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import tiimi3.KoiranVaatteet.domain.Vaate;
+import tiimi3.KoiranVaatteet.domain.Valmistaja;
 import tiimi3.KoiranVaatteet.domain.ValmistajaRepository;
 
 @Controller
@@ -13,11 +16,27 @@ public class ValmistajaController {
 	@Autowired
 	private ValmistajaRepository vrepository;
 	
-	@RequestMapping(value = "valmistajat", method = RequestMethod.GET)
+	@RequestMapping(value = "/valmistajat", method = RequestMethod.GET)
 	public String valmistajat(Model model) {
 		model.addAttribute("valmistajat", vrepository.findAll());
 		return "valmistajat";
 	}
 	
-
+	@RequestMapping(value = "/valmistajat/poista/{id}", method = RequestMethod.GET)
+	public String deleteValmistaja(@PathVariable("id") Long valmistajaid, Model model) {
+		vrepository.deleteById(valmistajaid);
+		return "redirect:../../valmistajat";
+	}
+	
+	@RequestMapping(value = "/valmistajat/lisaa", method = RequestMethod.GET)
+	public String addValmistaja(Model model) {
+		model.addAttribute("valmistaja", new Valmistaja());
+		return "lisaavalmistaja";
+	}
+	
+	@RequestMapping(value = "/valmistajat/tallenna", method = RequestMethod.POST)
+	public String saveValmistaja(Valmistaja valmistaja){
+	    vrepository.save(valmistaja);
+	    return "redirect:../valmistajat";
+	}
 }
