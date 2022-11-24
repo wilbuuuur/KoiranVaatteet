c49d541501faa3d941044d9c32841b680646ae9e
wilbuuuur <112497423+wilbuuuur@users.noreply.github.com>
2022-11-24 13:37:57 +0200

    Update RestVaateController.java

--- src/main/java/tiimi3/KoiranVaatteet/web/RestVaateController.java
+++ src/main/java/tiimi3/KoiranVaatteet/web/RestVaateController.java
@@ -1,41 +1,46 @@
 package tiimi3.KoiranVaatteet.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import tiimi3.KoiranVaatteet.domain.KoiranVaatteetRepository;
 import tiimi3.KoiranVaatteet.domain.Vaate;
+import tiimi3.KoiranVaatteet.domain.ValmistajaRepository;
+import tiimi3.KoiranVaatteet.domain.Valmistaja;
 
-@Controller
+@RestController
 public class RestVaateController {
 
 	@Autowired
 	private KoiranVaatteetRepository repository;
+	
+	@Autowired
+	private ValmistajaRepository vrepo;
 
-	@GetMapping("/api/vaatteet")
+	@GetMapping("/rest/vaatteet")
 	public Iterable<Vaate> getVaatteet() {
 		return repository.findAll();
 	}
 
-	@PostMapping("/api/vaatteet")
+	@PostMapping("/rest/vaatteet")
 	Vaate newVaate(@RequestBody Vaate newVaate) {
 		return repository.save(newVaate);
 
 	}
 
-	@PutMapping("/api/vaatteet/{id}")
+	@PutMapping("/rest/vaatteet/{id}")
 	Vaate editVaate(@RequestBody Vaate muokattuVaate, @PathVariable Long id) {
 		muokattuVaate.setId(id);
 		return repository.save(muokattuVaate);
 	}
 
-	@DeleteMapping("api/cars/{id}")
+	@DeleteMapping("/rest/vaatteet/{id}")
 	void deleteVaate(@PathVariable Long id) {
 		repository.deleteById(id);
 
@@ -46,17 +51,35 @@ public class RestVaateController {
 		System.out.println("restFindVaateByName metodissa");
 		return repository.findByName(name);
 	}
-	// REST etsii kaikki vaatteet
-	// @RequestMapping(value="/api/vaatteet", method = RequestMethod.GET)
-	// public @ResponseBody List<Vaate> listVaateRest() {
-	// return (List<Vaate>) repository.findAll();
-	// }
+	
+	@GetMapping("/rest/valmistajat")
+	public Iterable<Valmistaja> getValmistajat() {
+		return vrepo.findAll();
+	}
 
-	// Rest etsii vaatteen id:llä
-	// @RequestMapping(value="/api/vaate/{id}", method = RequestMethod.GET)
-	// public @ResponseBody Optional<Vaate> findVaateRest(@PathVariable("id") Long
-	// vaateId) {
-	// return repository.findById(vaateId);
-	// }
+	@PostMapping("/rest/valmistajat")
+	Valmistaja newValmistaja(@RequestBody Valmistaja newValmistaja) {
+		return vrepo.save(newValmistaja);
+
+	}
+
+	@PutMapping("/rest/valmistajat/{id}")
+	Valmistaja editValmistaja(@RequestBody Valmistaja muokattuValmistaja, @PathVariable Long id) {
+		muokattuValmistaja.setId(id);
+		return vrepo.save(muokattuValmistaja);
+	}
+
+	@DeleteMapping("/rest/valmistajat/{id}")
+	void deleteValmistaja(@PathVariable Long id) {
+		vrepo.deleteById(id);
+
+	}
+
+	@GetMapping("/restFindValmistajaByName/{name}")
+	Iterable<Valmistaja> findValmistajaByName(@PathVariable("name") String name) {
+		return vrepo.findByName(name);
+	}
+	
+	
 
 }
