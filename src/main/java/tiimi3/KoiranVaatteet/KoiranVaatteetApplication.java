568385aab2a7c4d5b32c0adbfabe59ec4b154e95
wilbuuuur <ville.aalto@myy.haaga-helia.fi>
2022-12-01 15:10:10 +0200

    Update KoiranVaatteetApplication.java

--- src/main/java/tiimi3/KoiranVaatteet/KoiranVaatteetApplication.java
+++ src/main/java/tiimi3/KoiranVaatteet/KoiranVaatteetApplication.java
@@ -21,8 +21,8 @@ public class KoiranVaatteetApplication {
 	@Bean
 	public CommandLineRunner demo(UserRepository urepository) {
 		return (args) -> {
-			User user1 = new User("user_test", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
-			User user2 = new User("premium_test", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C",
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C",
 					"ADMIN");
 			urepository.save(user1);
 			urepository.save(user2);
