79ad694ed24821ba7e8dfb579b4f4b604d983d98
jesreu <jesse.reunamo@gmail.com>
2022-12-01 14:28:03 +0200

    Lisätty websecurity ja userdetailservice

--- src/main/java/tiimi3/KoiranVaatteet/WebSecurityConfig.java
+++ src/main/java/tiimi3/KoiranVaatteet/WebSecurityConfig.java
@@ -1,1 +1,43 @@
 package tiimi3.KoiranVaatteet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import tiimi3.KoiranVaatteet.web.UserDetailService;
+
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailService userDetailsService;	
+	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .authorizeRequests().antMatchers("/**").permitAll() // Enable css when logged out
+        .and()
+        .authorizeRequests()
+          .anyRequest().authenticated()
+          .and()
+      .formLogin()
+          .defaultSuccessUrl("/", true)
+          .permitAll()
+          .and()
+      .logout()
+          .permitAll();
+    }
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+}
