603b3b1e16f8681b30c600abdc942dd6ad54da8b
wilbuuuur <ville.aalto@myy.haaga-helia.fi>
2022-12-11 16:56:18 +0200

    Update WebSecurityConfig.java

--- src/main/java/tiimi3/KoiranVaatteet/WebSecurityConfig.java
+++ src/main/java/tiimi3/KoiranVaatteet/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package tiimi3.KoiranVaatteet;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,23 +9,35 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import tiimi3.KoiranVaatteet.web.UserDetailService;
 
-@SuppressWarnings("deprecation")
+
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig  {
 	@Autowired
 	private UserDetailService userDetailsService;
 
 	// Määritellään sallitut endpointit
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/css/**" , "/login", "/logout", "/api/**", "/rest/**").permitAll() // Enable css when logged out
-				.and().authorizeRequests().anyRequest().authenticated().and().formLogin().defaultSuccessUrl("/", true)
-				.permitAll().and().logout().permitAll().and().exceptionHandling().accessDeniedPage("/errorsivu");
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/", "/css/**" , "/login", "/logout", "/api/**", "/rest/**").permitAll() // Enable css when logged out
+				.and()
+				.authorizeRequests()
+				.anyRequest().authenticated()
+				.and().formLogin().defaultSuccessUrl("/", true)
+				.permitAll()
+				.and()
+				.logout()
+				.permitAll()
+				.and()
+				.exceptionHandling().accessDeniedPage("/errorsivu");
+		return http.build();
 	}
 
 	@Autowired
